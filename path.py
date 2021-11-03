d59fa72fd2d59406ad495d70cf9c48b94d1dfb0e
EEKIM10 <eek@clicksminuteper.net>
2021-11-03 11:53:12 +0000

    Fix path mender.

--- path.py
+++ path.py
@@ -10,11 +10,10 @@ scripts_dir = (us / ".." / "scripts").resolve()
 scripts = str(scripts_dir)
 if not userpath.in_current_path(scripts):
     userpath.append(scripts)
+    if userpath.in_new_path(scripts):
+        print("Added scripts directory to path.")
 else:
     print("Scripts directory is already in path.")
-
-if userpath.in_new_path(scripts):
-    print("Scripts directory is now in path.")
 
 if userpath.need_shell_restart(scripts):
     print("You need to restart your shell for this to take effect.")
