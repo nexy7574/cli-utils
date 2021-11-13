690105685fa4b9a9b9147e67cc0dc81376dc7e1b
EEKIM10 <eek@clicksminuteper.net>
2021-11-13 10:10:58 +0000

    Make path.py look nice

--- path.py
+++ path.py
@@ -3,17 +3,25 @@
 This simple meta script just adds the `script` directory to path.
 """
 import userpath
+from rich.console import Console
 from pathlib import Path
 
 us = Path(__file__)
 scripts_dir = (us / ".." / "scripts").resolve()
 scripts = str(scripts_dir)
-if not userpath.in_current_path(scripts):
-    userpath.append(scripts)
-    if userpath.in_new_path(scripts):
-        print("Added scripts directory to path.")
-else:
-    print("Scripts directory is already in path.")
+console = Console()
+print = console.print
 
-if userpath.need_shell_restart(scripts):
-    print("You need to restart your shell for this to take effect.")
+with console.status("Checking if scripts directory is in path") as status:
+    if not userpath.in_current_path(scripts):
+        status.update("Adding scripts directory to path")
+        userpath.append(scripts)
+        status.update("Checking if scripts directory is now in path")
+        if userpath.in_new_path(scripts):
+            print("Added scripts directory to path.")
+    else:
+        print("Scripts directory is already in path.")
+
+    status.update("Checking if shell needs restarting")
+    if userpath.need_shell_restart(scripts):
+        print("You need to restart your shell for this to take effect.")
