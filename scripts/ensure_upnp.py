52ef1c6802c95444b012243815860184c3de1944
EEKIM10 <eek@clicksminuteper.net>
2022-05-25 19:46:36 +0100

    Brackets are a pain in the ass

--- scripts/ensure_upnp.py
+++ scripts/ensure_upnp.py
@@ -21,11 +21,10 @@ from rich.prompt import IntPrompt
 
 console = Console()
 console.log("Interactive terminal." if sys.__stdin__.isatty() else "Non-interactive terminal.")
-# install(console=console, extra_lines=3, show_locals=True)
 home_dir = Path(__file__).parent
 
 
-our_ip = os.getenv("IP", socket.gethostbyname(socket.gethostname()) + ".local")
+our_ip = os.getenv("IP", socket.gethostbyname(socket.gethostname() + ".local"))
 
 console.log("Forwarding traffic to %s." % our_ip)
 
