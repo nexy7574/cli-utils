0856fe7dbeb86ac9d714564015680e678ecde156
eek7574 <nexy7574@icloud.com>
2022-11-14 19:17:49 +0000

    Allow for pipx installation via setup.py
    Signed-off-by: eek7574 <nexy7574@icloud.com>

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from subprocess import run
+
+__VERSION__ = (
+    "0.1.0+g"
+    + run(("git", "rev-parse", "--short", "HEAD"), capture_output=True, encoding="utf-8", check=True).stdout.strip()
+)
+
+with open("./requirements.txt") as file:
+    requirements = file.read().splitlines()
+
+setup(
+    name="cli-utils",
+    version=__VERSION__,
+    packages=["scripts"],
+    url="https://github.com/EEKIM10/cli-utils",
+    license="GNU General Public License v3.0",
+    author="nex",
+    author_email="",
+    description="A set of CLI tools that I use",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "ensure-upnp = scripts.ensure_upnp:main",
+            "systemd-gen = scripts.systemd_gen:main",
+            "upnp-revoker = scripts.upnp_revoker:main",
+            "revoke-upnp = scripts.upnp_revoker:main",
+            "visual-rm = scripts.visual_rm:main",
+            "vrm = scripts.visual_rm:main",
+        ]
+    },
+)
