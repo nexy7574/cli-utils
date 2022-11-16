48c4010ed2a45fe97994ae4284b906aa74f2ab38
eek7574 <53234499+EEKIM10@users.noreply.github.com>
2022-11-16 23:13:10 +0000

    [tests] removed tests, unused.

--- scripts/tests/test_systemd_gen.py
+++
@@ -1,4 +0,0 @@
-def test_import():
-    from .. import systemd_gen
-
-    assert systemd_gen.__file__.endswith(".py")
--- scripts/tests/test_upnp_revoker.py
+++
@@ -1,8 +0,0 @@
-def test_import():
-    try:
-        from .. import upnp_revoker
-    except ImportError:
-        return False
-
-    assert upnp_revoker.__file__.endswith(".py"), "the hell"
-    assert hasattr(upnp_revoker, "print_ports"), "No function thingy"
