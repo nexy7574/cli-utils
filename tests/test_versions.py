35276563d459ce1365ead10c68063c7d85d21a3d
eek7574 <me@nexy7574.cyou>
2022-11-17 08:39:40 +0000

    Fix tests

--- tests/test_versions.py
+++ tests/test_versions.py
@@ -1,9 +1,10 @@
 import pytest
+import importlib
 
 
 @pytest.mark.parametrize("module", ["ensure_upnp", "hashgen", "systemd_gen", "upnp_revoker", "visual_rm"])
 def test_import(module: str):
     try:
-        __import__("scripts." + module)
+        importlib.import_module("scripts." + module, "scripts")
     except ImportError as e:
         assert False, e
