c7d9dc0f30ba7a32dab3d7c36e088b3ab7ebcaf0
eek7574 <me@nexy7574.cyou>
2022-12-15 09:14:52 +0000

    Move arriva dispatcher bash script to utils since its not a normal script
    Signed-off-by: eek7574 <me@nexy7574.cyou>

--- scripts/utils/.arriva-dispatcher.bash
+++ scripts/utils/.arriva-dispatcher.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # This file will not be installed by pipx!
 # Please run the following command:
-#     sudo wget -O /etc/NetworkManager/dispatcher.d/arriva-dispatcher.bash https://github.com/EEKIM10/cli-utils/raw/master/scripts/.arriva-dispatcher.bash \
+#     sudo wget -O /etc/NetworkManager/dispatcher.d/arriva-dispatcher.bash https://github.com/EEKIM10/cli-utils/raw/master/scripts/utils/.arriva-dispatcher.bash \
 #     && sudo chmod +x /etc/NetworkManager/dispatcher.d/arriva-dispatcher.bash
 
 if [[ "$2" == "up" && "$1" == "wlan0" ]]; then
