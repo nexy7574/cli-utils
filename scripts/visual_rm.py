dbdf43782bf60abadf02aa3d84a6e1685be827c6
nex <me@nexy7574.cyou>
2023-01-31 17:59:47 +0000

    Fix vrm thread hang
    Signed-off-by: nex <me@nexy7574.cyou>

--- scripts/visual_rm.py
+++ scripts/visual_rm.py
@@ -160,6 +160,11 @@ class Main:
                         deleted=self.deleted_directories + self.deleted_files,
                     )
                     done_threads = len(threads) - sum(t.is_alive() for t in threads)
+                    for thread in threads:
+                        if thread.is_alive() is False:
+                            threads.remove(thread)
+                            del thread
+                            # Thread is dead, remove it from the list
                     progress.update(
                         threads_task,
                         total=len(threads),
