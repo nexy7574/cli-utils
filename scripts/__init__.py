394bbbed49c8395d1fb1af1b9be33bb31b324b60
nex <me@nexy7574.co.uk>
2023-04-07 11:17:04 +0100

    Bump minimum python version in version check
    Signed-off-by: nex <me@nexy7574.co.uk>

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
 
-if sys.version_info.major != 3 or sys.version_info.minor <= 4:
+if sys.version_info.major != 3 or sys.version_info.minor <= 9:
     print("You have an unsupported python version! Please upgrade.")
     sys.exit(1)
